c8be86127923048f2b672aff9aea3a486ffd31b4
antonin <antonin.fille@epitech.eu>
2022-03-02 13:13:24 +0100

    setup boid

---
+++ src/Boid.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2022
+** testSfml
+** File description:
+** Boid
+*/
+
+#ifndef BOID_HPP_
+#define BOID_HPP_
+
+class Boid {
+public:
+    Boid();
+    ~Boid();
+
+protected:
+private:
+};
+
+#endif /* !BOID_HPP_ */
